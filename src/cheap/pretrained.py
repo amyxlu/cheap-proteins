8b032463908461c03b7adc95e64597384416936e
Amy X. Lu <lux70@gene.com>
2024-08-08 19:48:50 +0000

    update API to load directly from model ID

--- src/cheap/pretrained.py
+++ src/cheap/pretrained.py
@@ -6,42 +6,49 @@ import torch
 from .esmfold import esmfold_v1_embed_only
 from .model import HourglassProteinCompressionTransformer
 from .constants import CATH_COMPRESS_LEVEL_TO_ID, CHECKPOINT_DIR_PATH
+from .typed import PathLike
 
 
 def load_pretrained_model(
     shorten_factor=1,
     channel_dimension=1024,
     model_dir=CHECKPOINT_DIR_PATH,
-    infer_mode=True,
+    infer_mode=True
 ):
     if (shorten_factor == 1) and (channel_dimension == 1024):
         return esmfold_v1_embed_only()
-
     else:
-        # load saved lightning checkpoint
         model_id = CATH_COMPRESS_LEVEL_TO_ID[shorten_factor][channel_dimension]
-        checkpoint_fpath = model_dir / model_id / "last.ckpt"
-        ckpt = torch.load(checkpoint_fpath)
+        return load_model_from_id(model_id=model_id, model_dir=model_dir, infer_mode=infer_mode)
 
-        # initialize model based on saved hyperparameters
-        init_hparams = ckpt["hyper_parameters"]
-        keys_to_ignore = ["latent_scaler", "seq_emb_fn"]
-        for k in keys_to_ignore:
-            try:
-                init_hparams.pop(k)
-            except KeyError:
-                pass
 
-        # load state dict
-        model = HourglassProteinCompressionTransformer(
-            **init_hparams, force_infer=infer_mode
-        )
-        model.load_state_dict(ckpt["state_dict"])
-        if infer_mode:
-            model.eval()
-            model.requires_grad_(False)
+def load_model_from_id(
+    model_id: str,
+    model_dir: PathLike = CHECKPOINT_DIR_PATH,
+    infer_mode: bool = True,
+):
+    checkpoint_fpath = model_dir / model_id / "last.ckpt"
+    ckpt = torch.load(checkpoint_fpath)
 
-        return model
+    # initialize model based on saved hyperparameters
+    init_hparams = ckpt["hyper_parameters"]
+    keys_to_ignore = ["latent_scaler", "seq_emb_fn"]
+    for k in keys_to_ignore:
+        try:
+            init_hparams.pop(k)
+        except KeyError:
+            pass
+
+    # load state dict
+    model = HourglassProteinCompressionTransformer(
+        **init_hparams, force_infer=infer_mode
+    )
+    model.load_state_dict(ckpt["state_dict"])
+    if infer_mode:
+        model.eval()
+        model.requires_grad_(False)
+
+    return model
 
 
 def CHEAP_shorten_1_dim_1024(infer_mode=True, model_dir=CHECKPOINT_DIR_PATH):
